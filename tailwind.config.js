d984c7ae05ebc75f58249bb80dd95a2ed7ed0f97
TheChrisP <christoph.pabel@gmail.com>
2021-11-22 20:39:01 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        BluePale: "hsl(225, 100%, 94%)",
+        BlueBright: "hsl(245, 75%, 52%)",
+        BlueVeryPale: "hsl(225, 100%, 98%)",
+        BlueDesaturated: "hsl(224, 23%, 55%)",
+        BlueDark: "hsl(223, 47%, 23%)",
+      },
+      fontFamily: {
+        body: ["Red Hat Display"],
+      },
+      backgroundImage: (theme) => ({
+        img_mobile: "url('/images/pattern-background-mobile.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
